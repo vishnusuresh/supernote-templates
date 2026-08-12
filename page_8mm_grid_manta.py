037e3cb80b5be743ea2cbe5c188c6bcbd0ccb859
vishnusuresh <vishnusuresh@users.noreply.github.com>
2026-08-12 11:37:13 +0100

    manta: 8mm grid
    This script generates an 8mm grid in SVG format using specified screen dimensions for Manta.

---
+++ page_8mm_grid_manta.py
@@ -0,0 +1,35 @@
+from textwrap import dedent
+
+import svg
+
+from supernote_manta import MM, SCREEN_WIDTH, SCREEN_HEIGHT, border
+from utils import generate_grid, generate_dot_grid
+
+
+grid_size = 8.0 * MM
+grid_width = int(SCREEN_WIDTH // grid_size)
+grid_height = int(SCREEN_HEIGHT // grid_size)
+
+top_corner = (
+    (SCREEN_WIDTH - grid_width * grid_size) / 2,
+    (SCREEN_HEIGHT - grid_height * grid_size) / 2,
+)
+
+
+canvas = svg.SVG(
+    width=SCREEN_WIDTH,
+    height=SCREEN_HEIGHT,
+    elements=[
+        # border(),
+        generate_grid(
+            top_corner[0],
+            top_corner[1],
+            grid_size,
+            grid_width,
+            grid_height,
+            fill="#888888",
+        ),
+    ],
+)
+
+print(canvas)
